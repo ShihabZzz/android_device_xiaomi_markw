b6d33ff4253081ab89444d7ba33352b5989e7327
Matadeen Mishra <matade@codeaurora.org>
2016-10-14 10:57:20 +0530

    BT: cleanup of unused FLAG

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -27,15 +27,9 @@
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 
-#define BTM_WBS_INCLUDED TRUE
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-
 // Wide-band speech support
 #define BTIF_HF_WBS_PREFERRED TRUE
 
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
 #define BT_CLEAN_TURN_ON_DISABLED 1
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
