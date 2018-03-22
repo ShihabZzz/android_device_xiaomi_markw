8affcceb1e96511a9b240653f44dff16faa4ca4f
TheScarastic <warabhishek@gmail.com>
2018-03-22 23:37:14 +0530

    markw: Import xiaomidoze from xiaomi/msm8996

--- device.mk
+++ device.mk
@@ -168,6 +168,10 @@ PRODUCT_PACKAGES += \
     libgenlock \
     libtinyxml
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
