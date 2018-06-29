95045477c89aba6895fce4004f13860890df46f7
LuK1337 <priv.luk@gmail.com>
2018-06-29 18:44:41 +0200

    msm8953-common: Remove camera.device@3.2-impl from PRODUCT_PACKAGES
    * camera.device@3.2-impl is a dependency of camera provider
      therefore there's no need to add it to PRODUCT_PACKAGES.

--- device.mk
+++ device.mk
@@ -212,8 +212,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    camera.device@3.2-impl
+    android.hardware.camera.provider@2.4-service 
 
 # Configstore
 PRODUCT_PACKAGES += \
