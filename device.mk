cd5dd07da0ae302038b51c39dfa9cf345256a560
Artem Borisov <dedsa2002@gmail.com>
2018-12-24 15:25:51 +0400

    msm8953-common: Add Missing Packages Of QCOM's WFD
    * Sadly, out-of-the-box Miracast support was killed in P, so
      we need this to keep Wi-Fi display feature working.
    
    Change-Id: Id680d918b64c0e3733251db45992ec477b6fbdfb

--- device.mk
+++ device.mk
@@ -98,6 +98,7 @@ PRODUCT_PACKAGES += \
     audio_amplifier.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
+	 libaacwrapper \
     libqcompostprocbundle \
     libaudio-resampler \
     libqcomvisualizer \
@@ -381,7 +382,7 @@ PRODUCT_PACKAGES += \
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp_policy/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy 
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
@@ -461,12 +462,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny \
     $(LOCAL_PATH)/configs/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf
 
-# Wfd
-PRODUCT_PACKAGES += \
-    libnl
-
+# Wi-Fi Display
 PRODUCT_BOOT_JARS += \
-    WfdCommon
+		WfdCommon
 
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/markw/markw-vendor.mk)
