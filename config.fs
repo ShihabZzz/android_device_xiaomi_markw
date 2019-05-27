b38740dc2288e849915a429dcba1f97f4c617a9e
ZeeLog <Danjel.Zammit@gmail.com>
2019-05-27 13:48:48 +0200

    markw: cleanup config.fs
    * remove unused entries

--- config.fs
+++ config.fs
@@ -58,12 +58,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/pd-mapper]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
 [system/vendor/bin/pd-mapper]
 mode: 0755
 user: AID_SYSTEM
@@ -135,12 +129,6 @@ mode: 0755
 user:  AID_GPS
 group: AID_GPS
 caps: SETUID SETGID
-
-[vendor/bin/xtwifi-client]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [vendor/bin/sensors.qti]
 mode: 0755
