7784a02443a72eb67ce52a8c2672cf79b1a5a625
ShihabZzz <shihab.riadn@gmail.com>
2019-06-14 12:13:23 +0600

    markw: Remove Duplicate Local_Path

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -35,7 +35,6 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
 
 # fake print lib for hexedited fingerprint libs
-LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SHARED_LIBRARIES := \
