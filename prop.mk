d92b78e05f956a7147f9afa3fc850504345c229d
Murat Kozan <muratkozan350@gmail.com>
2019-06-25 12:44:16 +0300

    markw: Use persist time services

--- prop.mk
+++ prop.mk
@@ -180,7 +180,7 @@ service.qti.ims.enabled=1
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.timed.enable=true
+persist.vendor.delta_time.enable=true
 
 # Tcp
 PRODUCT_PROPERTY_OVERRIDES += \
