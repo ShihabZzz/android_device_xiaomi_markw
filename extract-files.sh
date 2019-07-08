f895a184bc54d4ec4a6dd700832214dfe1933d0e
Quallenauge <Hamsi2k@freenet.de>
2019-07-08 10:05:07 +0200

    markw: extract-files: Use keymaster@3.0-impl from Oreo.
    Some apps would store their keys via the keystore way.
    Since Android P release this isn't sometimes possible
    where in Android O it was for this device.
    
    The keystore returns in such cases
    W//vendor/bin/hw/android.hardware.keymaster@3.0-service(447): Digest 1 requested but not supported by KM1 hal
    I/keystore(832): -12 1
    E/AbstractAESEncryptor(426): java.security.InvalidKeyException: Keystore operation failed
    ...
    E/AbstractAESEncryptor(426): Caused by: android.security.KeyStoreException: Unsupported digest
    
    This doesn't occur when switching to android.hardware.keymaster@3.0-impl
    from Android O stock release.
    
    To not conflict with upstream dependend libraries
    * libsoftkeymasterdevice.so
    * libkeymaster_messages.so
    * libkeymaster_portable.so
    * libkeymaster_staging.so
    * libsoftkeymaster.so
    rename and patch them up so that they only used by this hal.
    
    Change-Id: Icb390056a8799ef7ac3fb294a07955e60eb1668e
    Signed-off-by: ShihabZzz <shihab.riadn@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -68,7 +68,16 @@ if [ -s "$MY_DIR"/proprietary-files.txt ]; then
     extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
     DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
-    
+
+;;
+        vendor/lib64/hw/android.hardware.keymaster@3.0-impl.so|vendor/lib64/libsoftkeymasterdevice-v27.so|vendor/lib64/libkeymaster_messages-v27.so|vendor/lib64/libkeymaster_portable-v27.so|vendor/lib64/libkeymaster_staging-v27.so|vendor/lib64/libsoftkeymaster-v27.so)
+                patchelf --replace-needed "libsoftkeymasterdevice.so" "libsoftkeymasterdevice-v27.so" "${2}"
+                patchelf --replace-needed "libkeymaster_messages.so" "libkeymaster_messages-v27.so" "${2}"
+                patchelf --replace-needed "libkeymaster_portable.so" "libkeymaster_portable-v27.so" "${2}"
+                patchelf --replace-needed "libkeymaster_staging.so" "libkeymaster_staging-v27.so" "${2}"
+                patchelf --replace-needed "libsoftkeymaster.so" "libsoftkeymaster-v27.so" "${2}"
+                patchelf --set-soname $(basename "${2}") "${2}"
+        ;;
 fi
 
 "$MY_DIR"/setup-makefiles.sh
