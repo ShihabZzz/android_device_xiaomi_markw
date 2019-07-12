0ffe97b7cd52a5fbf17c30ffd1073fa0497622fd
ShihabZzz <shihab.riadn@gmail.com>
2019-07-12 21:07:30 +0600

    markw : update build fingerprint

--- havoc_markw.mk
+++ havoc_markw.mk
@@ -40,8 +40,8 @@ PRODUCT_MODEL := Redmi 4
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V10.2.1.0.MBEMIXM release-keys" \
+    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V10.2.2.0.MBEMIXM release-keys" \
     TARGET_DEVICE="markw"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V10.2.1.0.MBEMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V10.2.2.0.MBEMIXM:user/release-keys
