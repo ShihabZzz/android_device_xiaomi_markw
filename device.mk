c93919f501fb4c539e1bbc5de38157db6e1c40f3
althafvly <althafvly@gmail.com>
2019-09-25 00:33:39 +0600

    markw: Enable CCodec
    Fixes this-
    
    09-24 15:40:05.050 D/StagefrightPluginLoader(  870): Failed to load
    library: libstagefright_ccodec.so (dlopen failed: library
    "libstagefright_ccodec.so" not found)
    
    Signed-off-by: ShihabZzz <shihab.riadn@gmail.com>

--- device.mk
+++ device.mk
@@ -362,6 +362,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+#StagefrightCodec
+PRODUCT_PACKAGES += \
+    libstagefright_ccodec
+
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
