966505c2e38722d3f0eceb71403ebd90e0229dff
Michael Bestas <mkbestas@lineageos.org>
2019-10-04 23:30:05 +0600

    markw: audio_amplifier: Use generated kernel headers
    Change-Id: I2632fc234b23714fce4eb8cfe0da630819790cef

--- libaudioampl/Android.mk
+++ libaudioampl/Android.mk
@@ -6,19 +6,17 @@ LOCAL_C_INCLUDES += \
 	$(call project-path-for,qcom-audio)/hal/msm8916/ \
 	$(call project-path-for,qcom-audio)/hal/ \
 	$(call project-path-for,qcom-audio)/hal/audio_extn \
-$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 	external/tinyalsa/include \
 	external/tinycompress/include \
-	system/media/audio_route/include \
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+	system/media/audio_route/include
 
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libtinyalsa libtinycompress
-LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 LOCAL_SRC_FILES := audio_amplifier.c
 LOCAL_MODULE := audio_amplifier.msm8953
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_VENDOR_MODULE := true
+LOCAL_HEADER_LIBRARIES += libhardware_headers
 
 include $(BUILD_SHARED_LIBRARY)
