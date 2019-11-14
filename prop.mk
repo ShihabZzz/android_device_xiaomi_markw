31c91c7b38443c22aa255958a3ca66dc6d676177
ShihabZzz <shihab.riadn@gmail.com>
2019-11-14 14:51:13 +0600

    markw: Add ADB prop

--- prop.mk
+++ prop.mk
@@ -2,6 +2,13 @@
 # system.prop for markw
 #
 
+# ADB
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.debuggable=1 \
+persist.sys.usb.config=adb \
+ro.secure=0 \
+ro.adb.secure=0
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
 af.fast_track_multiplier=2 \
