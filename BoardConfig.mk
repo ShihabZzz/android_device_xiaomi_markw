f23bab467d921b4ee382efe51dbd1a6aa0134750
Rajshekar Eashwarappa <reashw@codeaurora.org>
2019-11-22 19:37:03 +0600

    markw: Set androidboot.usbconfigfs to false for kernel 3.18
    Updated the commandline to set androidboot.usbconfigfs to
    false for kernel 3.18 as it does not support usbconfigfs
    configuration.
    
    Change-Id: Iebdfa3000c19a4a5fb5f7e43354ee2e473f63687

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,7 @@ TARGET_USES_64_BIT_BINDER := true
 #TARGET_KERNEL_CONFIG := franco_markw_defconfig
 TARGET_KERNEL_CONFIG := markw_defconfig
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 firmware_class.path=/vendor/firmware_mnt/image
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=false
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
