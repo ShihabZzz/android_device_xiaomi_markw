c481f1144338941a65af0b07e1b170c67476badb
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-12-09 19:59:24 +0100

    markw: build libunwind for gx_fpd
    Change-Id: Idd9fbc85e508cf8a0ab67d012eb86ab3025f34f5

--- device.mk
+++ device.mk
@@ -186,7 +186,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_markw
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_markw \
+    libunwind
 
 # FM
 PRODUCT_PACKAGES += \
