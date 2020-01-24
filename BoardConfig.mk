c5927c486883a37734ad1ac656d4929034d2ee08
ShihabZzz <Shihab.riadn@gmail.com>
2020-01-25 02:39:19 +0600

    markw: Re-organize & cleanup
    *Remove unused light flag & permission
    *Remove unused media packages & extensions

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,9 +161,6 @@ DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 TARGET_INIT_VENDOR_LIB := libinit_markw
 TARGET_RECOVERY_DEVICE_MODULES := libinit_markw
 
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
 # Lockscreen Real Time Charging Current Values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
 
