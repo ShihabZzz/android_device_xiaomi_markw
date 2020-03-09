1ee56c710fed8c00bae5e14f6ff2a9801941528e
ZeeLog <Danjel.Zammit@gmail.com>
2020-03-10 01:44:55 +0600

    markw: biometrics: use prebuilt modified blobs
    * nuke biometrics
    * pin modified fp blobs
    * build android.hardware.biometrics.fingerprint@2.1 from source
    
    Co-Authored-By: ShihabZzz <shihab.riadn@gmail.com>
    Signed-off-by: ShihabZzz <Shihab.riadn@gmail.com>

--- device.mk
+++ device.mk
@@ -179,7 +179,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_markw
+    android.hardware.biometrics.fingerprint@2.1
 
 # FM
 PRODUCT_PACKAGES += \
