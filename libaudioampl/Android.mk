cabf34eb2664b91062b217ff2cbb56a06781a022
ShihabZzz <Shihab.riadn@gmail.com>
2020-03-12 18:33:01 +0600

    markw: libaudioampl: update makefile
    *Derp fix
    
    Signed-off-by: ShihabZzz <Shihab.riadn@gmail.com>

--- libaudioampl/Android.mk
+++ libaudioampl/Android.mk
@@ -8,12 +8,12 @@ LOCAL_C_INCLUDES += \
 	$(call project-path-for,qcom-audio)/hal/audio_extn \
 	external/tinyalsa/include \
 	external/tinycompress/include \
-	system/media/audio_route/include
+	system/media/audio_route/include \
 
 LOCAL_CFLAGS := -Wno-unused-parameter
 LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_MODULE := audio_amplifier.msm8953
+LOCAL_MODULE := audio_amplifier.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libtinyalsa libtinycompress
