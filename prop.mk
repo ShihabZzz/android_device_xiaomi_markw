2397d35a33c911523ae51223554732db5c611eaa
ShihabZzz <Shihab.riadn@gmail.com>
2020-03-17 23:20:01 +0600

    markw: Fix SurfaceFlinger prop derp
    Signed-off-by: ShihabZzz <Shihab.riadn@gmail.com>

--- prop.mk
+++ prop.mk
@@ -183,9 +183,9 @@ service.qti.ims.enabled=1
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.early_phase_offset_ns=1500000
-debug.sf.early_app_phase_offset_ns=1500000
-debug.sf.early_gl_phase_offset_ns=3000000
+debug.sf.early_phase_offset_ns=1500000 \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
 debug.sf.early_gl_app_phase_offset_ns=15000000
 
 # Time Services
