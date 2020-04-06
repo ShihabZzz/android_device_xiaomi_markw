ef0ff763787f19a803aad811fd8b7663115502b2
ShihabZzz <Shihab.riadn@gmail.com>
2020-04-07 02:11:00 +0600

    markw: display: Don't build libqdMetaData
    Signed-off-by: ShihabZzz <Shihab.riadn@gmail.com>

--- device.mk
+++ device.mk
@@ -161,7 +161,6 @@ PRODUCT_PACKAGES += \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
-    libqdMetaData \
     libqdMetaData.system \
     libdisplayconfig \
     liboverlay \
