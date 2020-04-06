b47d5d4f15e653b5de66b050d47f28d891bcd720
ShihabZzz <Shihab.riadn@gmail.com>
2020-04-06 10:28:23 +0600

    markw: audio_amplifier: Too much dependency!!!
    Signed-off-by: ShihabZzz <Shihab.riadn@gmail.com>

--- audio_amplifier/Android.mk
+++ audio_amplifier/Android.mk
@@ -1,22 +1,33 @@
+#
+# Copyright 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES += \
-	$(call project-path-for,qcom-audio)/hal/msm8916/ \
-	$(call project-path-for,qcom-audio)/hal/ \
-	$(call project-path-for,qcom-audio)/hal/audio_extn \
-	external/tinyalsa/include \
-	external/tinycompress/include \
-	system/media/audio_route/include \
+LOCAL_C_INCLUDES += $(call project-path-for,qcom-audio)/hal
+
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
 LOCAL_CFLAGS := -Wno-unused-parameter
-LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_MODULE := audio_amplifier.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libtinyalsa libtinycompress
+LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_SRC_FILES := audio_amplifier.c
 LOCAL_VENDOR_MODULE := true
 
