022f0f87c74642f815dda2739e8658a82ca8a899
Michael Bestas <mkbestas@lineageos.org>
2020-04-13 02:21:56 +0600

    markw: Stop copying WCNSS_qcom_cfg.ini to userdata
    Change-Id: I7b9fbc0e26a1563f3e13fe0f9d58308b618a5702

--- Android.mk
+++ Android.mk
@@ -83,15 +83,6 @@ $(RFS_MSM_SLPI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(RFS_MSM_ADSP_SYMLINKS) $(RFS_MSM_MPSS_SYMLINKS) $(RFS_MSM_SLPI_SYMLINKS)
 
-WCNSS_INI_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
-$(WCNSS_INI_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	      @echo "WCNSS config ini link: $@"
-	      @mkdir -p $(dir $@)
-	      @rm -rf $@
-	      $(hide) ln -sf /vendor/etc/wifi/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK)
-
 GOODIX_IMAGES := \
 	      goodixfp.b00 goodixfp.b01 goodixfp.b02 goodixfp.b03 \
 	      goodixfp.b04 goodixfp.b05 goodixfp.b06 goodixfp.mdt
