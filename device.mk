3296a8a9e31be35ae410b265c8f9769825e310f4
ZeeLog <Danjel.Zammit@gmail.com>
2020-04-14 09:32:01 +0200

    markw: Switch to default VR implementation
    Change-Id: I93e1e400fb0e240522be28a2e3e0f016ba6d2747

--- device.mk
+++ device.mk
@@ -383,12 +383,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
-# VR
-PRODUCT_PACKAGES += \
-    android.hardware.vr@1.0-impl \
-    android.hardware.vr@1.0-service \
-    vr.msm8953
-
 # Whitelisted apps
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
