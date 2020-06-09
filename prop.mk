a787c38f5c46c0b45d4937e0d0acacedd940060e
ZeeLog <Danjel.Zammit@gmail.com>
2020-06-09 12:32:21 +0600

    markw: Move/Add rild properties to system.prop
    * from CAF LA.UM.8.6.r1-04400-89xx.0
    
    Change-Id: I3485c0f8177d4be96bd648fe864d307615b08703

--- prop.mk
+++ prop.mk
@@ -173,11 +173,11 @@ persist.vendor.radio.hw_mbn_update=0 \
 persist.vendor.radio.rat_on=combine \
 persist.vendor.radio.sib16_support=1 \
 ril.subscription.types=NV,RUIM \
-rild.libargs=-d/dev/smd0 \
 rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,22 \
-service.qti.ims.enabled=1
+service.qti.ims.enabled=1 \
+vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
