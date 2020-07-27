a9f3853aa4f8c719e959cf10eb259e1a30ce18f4
ShihabZzz <Shihab.riadn@gmail.com>
2020-07-28 00:58:41 +0600

    markw: Nuke init module support
    Signed-off-by: ShihabZzz <Shihab.riadn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,10 +160,6 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_markw
-TARGET_RECOVERY_DEVICE_MODULES := libinit_markw
-
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
