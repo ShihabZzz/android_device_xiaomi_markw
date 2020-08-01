15ea9825df0642f812b1a844f098d3e83e8eb85b
baunilla <49725678+baunilla@users.noreply.github.com>
2020-08-01 22:03:43 +0600

    markw: Import custom LiveDisplay 2.0 HAL
    Pulled from LineageOS/android_hardware_lineage_livedisplay/sdm
    
    Cleaned up and adapted for PictureAdjustment (vendor) only:
    
    * Removed AdaptiveBacklight, DisplayModes, Constants, Types, Utils
    * Moved necessary constants to anonymous namespace
    
    Signed-off-by: ShihabZzz <Shihab.riadn@gmail.com>

--- device.mk
+++ device.mk
@@ -255,7 +255,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service.xiaomi_markw
 
 # Media
 PRODUCT_COPY_FILES += \
