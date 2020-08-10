61b7d89653dfbc70f6e4d8c481b00e8f1fe20956
ShihabZzz <Shihab.riadn@gmail.com>
2020-08-10 16:48:57 +0600

    markw: configs: Restore public libraries list from LA.UM.8.6.r1-04400-89xx.0
    This reverts commit 3b1a3a833e3d5d36d196a0d4565390484e6d6861.
    
    Signed-off-by: ShihabZzz <Shihab.riadn@gmail.com>

--- device.mk
+++ device.mk
@@ -268,6 +268,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Netutils
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0 \
