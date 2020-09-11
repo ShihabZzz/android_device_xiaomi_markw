3fd0494233f588390c216e7e0869f5183a790c82
ShihabZzz <Shihab.riadn@gmail.com>
2020-09-11 11:35:24 +0600

    markw: Remove prebuilt fingerprint support
    This reverts commit 1ee56c710fed8c00bae5e14f6ff2a9801941528e.
    
    Also explicitly applied these commit as well:
    
    * https://github.com/ShihabZzz/vendor_xiaomi_markw/commit/42c0c331a66db183ab69f645107b04fda46e42de
    * https://github.com/ShihabZzz/vendor_xiaomi_markw/commit/e12219b13a01584045ff83f1f18922d5a571a836

--- device.mk
+++ device.mk
@@ -178,7 +178,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_markw
 
 # FM
 PRODUCT_PACKAGES += \
